4955c9e2700b2d56707865892645a4a42880edf5
Diparsan79 <srpathak977+10@gmail.com>
2026-05-29 11:34:30 +0545

    im constantly learning about the bstacles of this game and how to implement all logics of attack and defense but for now i completed the board , now i gotta figure out how to shoot inside the board

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+def render(board_width,board_height, shots):
+    header= "+"+ "-" * board_width + "+"
+    print(header)
+    for i in range(board_height):
+        row = [  ]
+        print("|" + " " * board_width + "|")
+
+    print(header)
+
+    
+
+if __name__ =="__main__":
+    render(10,10, [(2,1),(3,1), (4,5), (8,1)])
+    inpt = input("where do you want to shoot?\n")
+    xstr,ystr = inpt.split(",")
+    x = int(xstr)
+    y = int(ystr)
+
+    
